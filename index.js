5c09b93a97b6fc14612be118293e16f433b8d9a0
Nobuyuki Kobayashi <nbr.41to@gmail.com>
2021-06-15 20:50:24 +0900

    .

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 export const sayHello = () => 'hello';
+
 export const sum = (a, b) => a + b;
+
+export const chengeObj = (array) => {
+  const obj = {};
+  array.map((item) => {
+    obj[item] = item;
+  });
+  return obj;
+};
+
+export const testReturn = (expect) => expect;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { sayHello, sum } from './index';
+import { sayHello, sum, chengeObj, testReturn } from './index';
 
 test('あいさつのテスト', () => {
   expect(sayHello()).toBe('hello');
@@ -10,4 +10,20 @@ test('計算のテスト', () => {
 
 test('計算のテスト 失敗', () => {
   expect(sum('123', 2)).not.toBe(125);
+});
+
+test('結果がオブジェクトのテスト', () => {
+  // console.log(chengeObj(['apple', 'banana']));
+  expect(chengeObj(['apple', 'banana'])).toEqual({ apple: 'apple', banana: 'banana' });
+  expect(chengeObj(['apple', 'banana'])).not.toBe({ apple: 'apple', banana: 'banana' }); // ≠ の判定になるので注意
+});
+
+test('真偽値系統のテスト', () => {
+  /* いろいろある */
+  expect(testReturn(null)).toBeNull(); // null である
+  expect(testReturn()).toBeUndefined(); // undefined である
+  expect(testReturn('hey yo')).toBeDefined(); // undefined ではない
+  expect(testReturn([])).toBeTruthy(); // true と判定されるもの
+  expect(testReturn('')).not.toBeTruthy(); // false と判定されるもの
+  expect(testReturn(0)).toBeFalsy(); // false と判定されるもの
 });
