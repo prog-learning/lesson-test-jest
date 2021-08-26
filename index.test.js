356a4749e74a5399413dd9c3a6f325d5823a93cc
nbr41to <nbr.41to@gmail.com>
2021-08-26 18:05:05 +0900

    .

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { sayHello, sum, chengeObj, testReturn } from './index';
+import { sayHello, sum, chengeObj } from './index';
 
 test('あいさつのテスト', () => {
   expect(sayHello()).toBe('hello');
@@ -14,16 +14,29 @@ test('計算のテスト 失敗', () => {
 
 test('結果がオブジェクトのテスト', () => {
   // console.log(chengeObj(['apple', 'banana']));
-  expect(chengeObj(['apple', 'banana'])).toEqual({ apple: 'apple', banana: 'banana' });
-  expect(chengeObj(['apple', 'banana'])).not.toBe({ apple: 'apple', banana: 'banana' }); // ≠ の判定になるので注意
+  expect(chengeObj(['apple', 'banana'])).toEqual({
+    apple: 'apple',
+    banana: 'banana',
+  });
+  expect(chengeObj(['apple', 'banana'])).not.toBe({
+    apple: 'apple',
+    banana: 'banana',
+  }); // ≠ の判定になるので注意
+});
+
+test('等価に関する検証', () => {
+  expect(['apple', 'banana']).toBe(['apple', 'banana']); // failed
+  expect(['apple', 'banana']).toEqual(['apple', 'banana']);
+  expect({ fruit: 'apple' }).toBe({ fruit: 'apple' }); // failed
+  expect({ fruit: 'apple' }).toEqual({ fruit: 'apple' });
 });
 
 test('真偽値系統のテスト', () => {
   /* いろいろある */
-  expect(testReturn(null)).toBeNull(); // null である
-  expect(testReturn()).toBeUndefined(); // undefined である
-  expect(testReturn('hey yo')).toBeDefined(); // undefined ではない
-  expect(testReturn([])).toBeTruthy(); // true と判定されるもの
-  expect(testReturn('')).not.toBeTruthy(); // false と判定されるもの
-  expect(testReturn(0)).toBeFalsy(); // false と判定されるもの
+  expect(null).toBeNull(); // null である
+  expect().toBeUndefined(); // undefined である
+  expect('hey yo').toBeDefined(); // undefined ではない
+  expect([]).toBeTruthy(); // true と判定されるもの
+  expect('').not.toBeTruthy(); // false と判定されるもの
+  expect(0).toBeFalsy(); // false と判定されるもの
 });
