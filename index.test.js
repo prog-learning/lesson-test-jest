013b7732d09fa84b9dc0a015de374b9444308b72
nbr41to <nbr.41to@gmail.com>
2021-08-26 19:13:26 +0900

    .

--- index.test.js
+++ index.test.js
@@ -54,9 +54,9 @@ describe('真偽値系統のテスト', () => {
 /* describe.each() でテストの引数をまとめる */
 // 公式引用: https://jestjs.io/ja/docs/api#describeeachtablename-fn-timeout
 describe.each([
-  [1, 1, 2],
-  [1, 2, 3],
-  [2, 1, 3],
+  [1, 1, 2], // 1 + 1 = 2
+  [1, 2, 3], // 1 + 2 = 3
+  [2, 1, 3], // 2 + 1 = 3
 ])('.add(%i, %i)', (a, b, expected) => {
   test(`returns ${expected}`, () => {
     expect(a + b).toBe(expected);
